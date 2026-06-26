91e48e8849bd4765371e99093003f0826755e967
filipecavinato <filipecavinato@hotmail.com>
2026-06-26 18:41:04 -0300

    Alterações de partes do código e do arquivo .gitignore

--- desafios/desafio27/__main__.py
+++ desafios/desafio27/__main__.py
@@ -1,14 +1,22 @@
 # Script criado por Filipe Cavinato
 
 from classes import Guerreiro, Mago
+from rich import print
 
 def main():
-    p1 = Guerreiro('Kratos', 2000)
+    p1 = Guerreiro('Kratos', 3000)
     p2 = Mago('Merlin', 3000)
+    contador = 1
 
-    p1.atacar(p2, 1000)
-    p2.curar()
-    p2.atacar(p1, 20000)
+    while p1.vivo and p2.vivo:
+        print(f'\n[bold yellow]Round {contador}:[/]')
+        contador += 1
+        p1.atacar(p2, 1000)
+        p2.curar()
+        p2.atacar(p1, 1000)
+        p1.curar()
+
+    p1.game_over(p2)
 
 if __name__ == '__main__':
     main()
--- desafios/desafio27/classes.py
+++ desafios/desafio27/classes.py
@@ -8,20 +8,49 @@ class Personagem(ABC):
     def __init__(self, nome, vida):
         self.nome = nome
         self.vida = vida
+        self.vida_max = vida
         self.golpes = []
+        self.vivo = True
+
+    def verifica_vida(self, alvo):
+        if self.vida <= 0:
+            self.vivo = False
+        elif alvo.vida <= 0:
+            alvo.vivo = False
 
     def atacar(self, alvo, forca):
-        print(f'[green]{self.nome}[/]({self.vida}) atacou [red]{alvo.nome}[/]({alvo.vida}) com [blue]{choice(self.golpes)}[/] de força {forca}')
-        dano = randint(0, forca)
-        alvo.receber_dano(dano)
-        print(f'[blue]{alvo.nome}[/] recebeu dano de [red]{dano}[/]!')
+        if self.vivo and alvo.vivo:
+            print(
+                f':crossed_swords: [green]{self.nome}[/]({self.vida}) atacou [red]{alvo.nome}[/]({alvo.vida}) com [blue]{choice(self.golpes)}[/] de força {forca}')
+            dano = randint(0, forca)
+            alvo.receber_dano(alvo, dano, forca)
+            self.verifica_vida(alvo)
 
-    def receber_dano(self, dano):
-        self.vida -= dano
+    def receber_dano(self, alvo, dano, forca):
+        if self.vivo:
+            if dano >= self.vida:
+                self.vida = 0
+            else:
+                self.vida -= dano
+
+        print(f':collision: [blue]{alvo.nome}[/] recebeu dano de [red]{dano}[/]! -> [red]{alvo.nome}({alvo.vida})[/]',
+              end='')
+        if dano == forca:
+            print('([purple]Critical Hit[/])')
+        else:
+            print()
+
+    def game_over(self, alvo):
+        print(f'\n[bold yellow]Fim de Batalha![/]')
+        if self.vivo:
+            print(f':trophy: [green]{self.nome}[/] Venceu de :skull: [red]{alvo.nome}[/] :skull:')
+        elif alvo.vivo:
+            print(f':trophy: [green]{alvo.nome}[/] Venceu de :skull: [red]{self.nome}[/]')
 
     @abstractmethod
     def curar(self):
         pass
+
 
 class Guerreiro(Personagem):
     def __init__(self, nome, vida):
@@ -29,9 +58,16 @@ class Guerreiro(Personagem):
         self.golpes = ['Soco', 'Pulo Giratório', 'Arremesso Preciso', 'Redemoinho do Caos']
 
     def curar(self):
-        cura = randint(0, 100)
-        print(f'[blue]{self.nome}[/] enrolou uma atadura nos ferimentos e [green]recuperou {cura} pontos[/] de vida.')
-        self.vida += cura
+        if self.vivo:
+            cura = randint(0, 100)
+            if self.vida_max >= self.vida + cura:
+                self.vida += cura
+                print(
+                    f':adhesive_bandage: [blue]{self.nome}[/] enrolou uma atadura nos ferimentos e [green]recuperou {cura} pontos[/] de vida. -> [green]{self.nome}({self.vida})[/]')
+
+            else:
+                self.vida = self.vida_max
+
 
 class Mago(Personagem):
     def __init__(self, nome, vida):
@@ -39,6 +75,11 @@ class Mago(Personagem):
         self.golpes = ['Bola de Fogo', 'Tornado de Fogo', 'Raio de Gelo', 'Míssil Magico']
 
     def curar(self):
-        cura = randint(0,100)
-        print(f'[blue]{self.nome}[/] fez uma magia de cura e [green]recuperou {cura} pontos[/] de vida.')
-        self.vida += cura
+        if self.vivo:
+            cura = randint(0, 100)
+            if self.vida_max >= self.vida + cura:
+                self.vida += cura
+                print(
+                    f':test_tube: [blue]{self.nome}[/] fez uma magia de cura e [green]recuperou {cura} pontos[/] de vida. -> [green]{self.nome}({self.vida})[/]')
+            else:
+                self.vida = self.vida_max
