ae61be2c4e1502c049fa62024de5eb786d23f64a
filipecavinato <filipecavinato@hotmail.com>
2026-07-08 15:43:22 -0300

    Criação do @property para alterar senha, criação do raise PermissionError e do tratamento de erro com Try/Except

--- desafios/desafio29/__main__.py
+++ desafios/desafio29/__main__.py
@@ -1,16 +1,35 @@
 # Script criado por Filipe Cavinato
 
 from classe import Diario
-from rich import inspect
+from rich import inspect, print
 
 def main():
     d = Diario()
-
     d.escrever('Primeira Mensagem')
     d.escrever('Você é uma pessoa simpática')
     d.escrever('Você gosta de Python')
-    d.ler('CeV!@')
-    inspect(d, private=True, methods=True)
+    try:
+        print(d.senha)
+    except PermissionError as erro:
+        print(f'[red] ERRO: {erro}[/]')
+
+    try:
+        d.ler(str(input('Digite a senha para ler o diario: ')).strip())
+    except PermissionError as erro:
+        print(f'[red] ERRO: {erro}[/]')
+
+    try:
+        print(f'[yellow]Alterando a senha[/]')
+        d.senha = str(input('Digite a nova senha: '))
+    except PermissionError as erro:
+        print(f'[red] ERRO: {erro}[/]')
+
+    try:
+        d.ler(str(input('Digite a senha para ler o diario: ')).strip())
+    except PermissionError as erro:
+        print(f'[red] ERRO: {erro}[/]')
+
+    #inspect(d, private=True, methods=True)   # Analise da Classe usando inspect
 
 if __name__ == '__main__':
     main()
--- desafios/desafio29/classe.py
+++ desafios/desafio29/classe.py
@@ -5,10 +5,11 @@ from rich import print
 class Diario:
     def __init__(self, senha_mestra = 'CeV!@'):
         self.__segredos = []
-        self.__senha = senha_mestra
+        self.__senha = senha_mestra.strip()
 
     def escrever(self, mensagem):
-        self.__segredos.append(mensagem)
+        if isinstance(mensagem, str) and len(mensagem) > 0:
+            self.__segredos.append(mensagem)
 
     def ler(self, senha = None):
         if senha == self.__senha:
@@ -16,5 +17,17 @@ class Diario:
             for segredo in self.__segredos:
                 print(f'- {segredo}')
         else:
-            print('[red] Senha Invalida! Você não pode ler o meu Diário[/]')
+            raise PermissionError('Senha Invalida! Você não pode ler o meu Diário.')
+
+    @property
+    def senha(self):
+        raise PermissionError(f'Ninguém tem permissão de ver a senha')
+
+    @senha.setter
+    def senha(self, nova_senha):
+        senha_antiga = str(input('Digite a senha atual para confirmar: ')).strip()
+        if senha_antiga == self.__senha:
+            self.__senha = nova_senha
+        else:
+            raise PermissionError(f'Senha Atual Invalida, você não pode alterar a senha!')
 
